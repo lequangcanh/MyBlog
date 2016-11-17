bad85258946027e0c8413ce826613633a29b05c9
lequangcanh <lequangcanh94@gmail.com>
2016-11-17 16:46:06 +0700

    Create Login/Logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  delete 'signout', to: 'sessions#destroy'
 
   resources :users
 end
