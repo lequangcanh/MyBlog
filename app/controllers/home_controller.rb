5f27aa3b64c9c79005c8884dac3a041cd50fc43a
lequangcanh <lequangcanh94@gmail.com>
2016-11-19 00:27:04 +0700

    Create index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @user = current_user
+    @entries = @user.feed.paginate(page: params[:page])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  # Returns a user's status feed.
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Entry.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+  
   # Follow a user
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
