0d96b66dfefb379380bf109b20da856242dc78e3
lequangcanh <lequangcanh94@gmail.com>
2016-11-29 08:53:35 +0700

    Add setting user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: :index
+  before_action :correct_user, only: :update
   before_action :admin_user, only: :destroy
 
   def index
@@ -22,6 +23,19 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -49,5 +63,10 @@ class UsersController < ApplicationController
 
   def admin_user
     redirect_to root_url unless current_user.admin?
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    redirect_to root_url unless @user == current_user
   end
 end
